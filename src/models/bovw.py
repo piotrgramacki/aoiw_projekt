acb717f1b2db41524ce11e2f3a8ef180f39c0d68
Szymon <wozniaks97@gmail.com>
2021-01-18 01:38:33 +0100

    optimize bovw for memory

--- src/models/bovw.py
+++ src/models/bovw.py
@@ -25,16 +25,7 @@ class BoVWRetriever:
         self.k_means = None
     
     def fit(self, data: TripletDataset):
-        x_train = np.empty(shape=(len(data), 256, 256, 3))
-        y_train = np.empty(shape=(len(data),), dtype=np.int)
-        for idx in trange(len(data)):
-            item = data[idx]
-
-            x_train[idx] = item["a"]
-            y_train[idx] = item["a_y"]
-
-        x_train_descriptors, y_train_descriptors = self.get_descriptors(x_train, y_train)
-        # x_train_descriptors, y_train_descriptors = self.get_descriptors_for_dataset(data, labels=True)
+        x_train_descriptors, y_train_descriptors = self.get_descriptors(data, labels=True)
         stacked_train_descriptors = np.vstack(x_train_descriptors)
         stacked_train_labels = np.hstack(y_train_descriptors)
         under_sampler = RandomUnderSampler(random_state=self.random_state)
@@ -56,21 +47,19 @@ class BoVWRetriever:
         self.k_means.fit(descriptors_for_kmeans)
     
     def eval(self, data: TripletDataset) -> float:
-        x_test = np.empty(shape=(len(data), 256, 256, 3))
         y_test = np.empty(shape=(len(data),), dtype=np.int)
 
         for idx in trange(len(data)):
             item = data[idx]
-            x_test[idx] = item["a"]
             y_test[idx] = item["a_y"]
         
 
-        x_test_encoded = self.encode_as_bovw(x_test)
+        x_test_encoded = self.encode_as_bovw(data)
 
         anmrr_value = anmrr(x_test_encoded, y_test[:, None], euclidean_distances)
         return anmrr_value
 
-    def get_descriptors_for_dataset(self, data: TripletDataset, labels=True):
+    def get_descriptors(self, data: TripletDataset, labels=True):
         desc = []
         matching_labels = []
         for idx in trange(len(data)):
@@ -94,49 +83,10 @@ class BoVWRetriever:
         else:
             return desc
 
-    def get_descriptors(
-        self, images: np.ndarray, labels: Union[np.ndarray, None] = None
-    ):
-        desc = []
-        matching_labels = []
+    def encode_as_bovw(self, data: TripletDataset) -> np.ndarray:
+        descriptors = self.get_descriptors(data, labels=False)
 
-        for idx, img in tqdm(
-            enumerate(images),
-            desc="Calculating SIFT descriptors",
-            total=images.shape[0],
-        ):
-            cv_img = img_as_ubyte(img)
-            cv_img = cv2.cvtColor(cv_img, cv2.COLOR_RGB2GRAY)
-            _, d = self.sift.detectAndCompute(cv_img, None)
-            if d is not None:
-                desc.append(d)
-
-                if labels is not None:
-                    matching_labels.append(np.repeat(labels[idx], len(d)))
-
-        if labels is not None:
-            return desc, matching_labels
-        else:
-            return desc
-
-    # def encode_dataset_as_bovw(self, data: TripletDataset) -> np.ndarray:
-    #     descriptors = self.get_descriptors_for_dataset(data, labels=False)
-
-    #     res = np.empty(shape=(len(data), self.k_means.n_clusters))
-
-    #     for idx, desc in tqdm(
-    #         enumerate(descriptors), total=len(descriptors), desc="Encoding as BOVW"
-    #     ):
-    #         words = self.k_means.predict(desc)
-    #         bovw, _ = np.histogram(words, bins=range(self.k_means.n_clusters + 1))
-    #         res[idx] = bovw / desc.shape[0]
-
-    #     return res
-
-    def encode_as_bovw(self, x: np.ndarray) -> np.ndarray:
-        descriptors = self.get_descriptors(x)
-
-        res = np.empty(shape=(x.shape[0], self.k_means.n_clusters))
+        res = np.empty(shape=(len(data), self.k_means.n_clusters))
 
         for idx, desc in tqdm(
             enumerate(descriptors), total=len(descriptors), desc="Encoding as BOVW"
